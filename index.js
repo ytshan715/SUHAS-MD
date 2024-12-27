93e1b5e713fa10bf457a03be57235f776e8403e0
KALANA-MD <loversmusic0016@gmail.com>
2024-12-27 08:36:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const axios = require('axios')
 const { File } = require('megajs')
 const prefix = '.'
 
-const ownerNumber = ['🎁 𝘼𝙙𝙙 𝙔𝙤𝙪𝙧 𝙊𝙬𝙣𝙚𝙧 𝙉𝙪𝙢𝙗𝙚𝙧 𝙃𝙚𝙧𝙚.🎁']
+const ownerNumber = ['94702932200']
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
