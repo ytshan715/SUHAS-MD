717594da67ea53663ba0740e6f1c03b1bd47635b
ytshan715 <bandarashanuka933@gmail.com>
2025-03-22 19:25:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const axios = require('axios')
 const { File } = require('megajs')
 const prefix = '.'
 
-const ownerNumber = ['94702932200']
+const ownerNumber = ['94740712026]
 
 //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
